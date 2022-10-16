8a8e34ea27219a3f1cdc8dfde56a229a9ea0c10b
unknown <zhou.he1@northeastern.edu>
2022-10-16 16:48:18 -0700

    Add express-test

--- exercises/express-test/server.js
+++ exercises/express-test/server.js
@@ -11,6 +11,14 @@ app.get('/dynamic.html', (request, response) => {
     response.send('This is not an actual file');
 });
 
+app.get('/dynamic.html', (req, res) => {
+    response.send('This is not an actual file');
+});
+
+app.get('/cats/:name', (req, res) => { 
+    console.log(req.params)
+});
+
 const cats = {
     
 }
