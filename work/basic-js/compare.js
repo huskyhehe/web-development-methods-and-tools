e3d5d76bacb8c244e1aa70e1af90a61469baf441
unknown <zhou.he1@northeastern.edu>
2022-10-17 12:29:57 -0700

    Submit basic-js

--- work/basic-js/compare.js
+++ work/basic-js/compare.js
@@ -5,6 +5,22 @@ module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 function compare( word, guess ) {  // DO NOT MODIFY
 
 /* YOU MAY MODIFY THE LINES BELOW */
+  const wordFrequency = countFrequencyOfCharacter(word);
+  const guessFrequency = countFrequencyOfCharacter(guess);
 
-  return 0; // this line is wrong
+  let matchCount = 0;
+  for (let char of Object.keys(guessFrequency)) {
+    if (char in wordFrequency) {
+      matchCount += Math.min(wordFrequency[char], guessFrequency[char]);
+    };
+  };
+  return matchCount;
+};
+
+function countFrequencyOfCharacter(string) {
+  let frequency = {};
+  for (let char of string.toUpperCase()) {
+    frequency[char] = frequency[char] + 1 || 1;
+  };
+  return frequency;
 }
